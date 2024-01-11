105a320852a85b50c100bf4c7ec5b73965f3175d
ameerj95 <ameerjamal.1995@gmail.com>
2024-01-11 10:26:44 +0200

    saved progress

--- backend/frontend/main.js
+++ backend/frontend/main.js
@@ -1,11 +1,8 @@
 console.log(" I am main js!!!");
 
 const add = (num1, num2) => {
-  console.log("Asad   !");
+  console.log("SOS   !");
   return num1 + num2;
 };
 
 
-const multiply = (num1, num2) => {
-    return num1*num2
-}
