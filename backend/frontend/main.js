ea597d7bfb1f9404aa1f7cb9c23969652b3550c2
ameerj95 <ameerjamal.1995@gmail.com>
2024-01-11 10:24:36 +0200

    added multiply, updated add function cosole

--- backend/frontend/main.js
+++ backend/frontend/main.js
@@ -1,6 +1,11 @@
 console.log(" I am main js!!!");
 
 const add = (num1, num2) => {
-  console.log("SOS   !");
+  console.log("Asad   !");
   return num1 + num2;
 };
+
+
+const multiply = (num1, num2) => {
+    return num1*num2
+}
